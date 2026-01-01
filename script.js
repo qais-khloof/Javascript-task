e7904e4fb19829df2767e114439f7c370c7752b3
Qais Khloof <q.khloof1@student.aaup.edu>
2026-01-01 21:48:12 +0200

    Filter buttons

--- script.js
+++ script.js
@@ -94,3 +94,15 @@ todoList.onchange = function (e) {
   }
 };
 
+for (var i = 0; i < filters.length; i++) {
+  filters[i].onclick = function () {
+    for (var j = 0; j < filters.length; j++) {
+      filters[j].classList.remove("active");
+    }
+
+    this.classList.add("active");
+    currentFilter = this.getAttribute("data-filter");
+    drawTasks();
+  };
+}
+
