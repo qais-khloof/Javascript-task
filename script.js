7144c6460ebdcb6291a6a5c5ca169aee552f62e7
mohammade Obaid <mbyd33034@gmail.com>
2026-01-01 20:27:37 +0200

    Edit and delete in list-JS

--- script.js
+++ script.js
@@ -66,3 +66,20 @@ function drawTasks() {
     todoList.appendChild(li);
   }
 }
+
+todoList.onclick = function (e) {
+  var t = e.target;
+
+  if (t.classList.contains("edit-btn")) {
+    editIndex = Number(t.getAttribute("data-index"));
+    editInput.value = todos[editIndex].text;
+    editModal.style.display = "block";
+    return;
+  }
+
+  if (t.classList.contains("delete-btn")) {
+    deleteIndex = Number(t.getAttribute("data-index"));
+    deleteModal.style.display = "block";
+    return;
+  }
+};
